bcadd8b2b61e81b3516ae4dec52804d8915590fe
izuua <izuuaaf@gmail.com>
2019-08-04 22:28:42 -0400

    Added the bamazon-manager.js file.

---
+++ bamazon-manager.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "bamazon_DB"
+})
+
+// View Products for Sale
+    
+//     * View Low Inventory
+    
+//     * Add to Inventory
+    
+//     * Add New Product
+
